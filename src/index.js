71d138545921594126eddc90857a6599ca8e35d3
JairoAmaya <hola@jairoamaya.co>
2025-10-28 13:52:53 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+/**
+ * Entry Point de la aplicación
+ * Contador 4.0 Express v2
+ */
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
